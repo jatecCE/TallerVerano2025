e508130eb4cd5d0afc23a780189a5d3079784008
Jason <jleiton@itcr.ac.cr>
2026-01-14 09:56:22 -0600

    [ADD] - agregando suma

--- MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/ExitActivity.java
+++ MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/ExitActivity.java
@@ -28,4 +28,7 @@ public class ExitActivity extends AppCompatActivity {
             System.exit(0); // Sale de la aplicación
         });
     }
+    public int suma(int a , int b){
+        return a+b;
+    }
 }
